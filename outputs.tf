c8ddb642ad931482710f73cd5fe94553cee28cfc
Daniel Wilcox <dmw@yubasolutions.com>
2020-02-08 21:33:23 +0000

    Add cloudfront hosted_zone_id to outputs.

--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,10 @@ output "cloudfront_hostname" {
   value = aws_cloudfront_distribution.hugo[0].domain_name
 }
 
+output "cloudfront_zone_id" {
+  value = aws_cloudfront_distribution.hugo[0].hosted_zone_id
+}
+
 output "s3_bucket_arn" {
   value = aws_s3_bucket.hugo.arn
 }
